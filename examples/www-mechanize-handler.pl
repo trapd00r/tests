c5c8ce0b73ed85ee953943af6893c4fd5b6b773b
Magnus Woldrich <m@japh.se>
2011-04-07 08:16:52 +0200

    add example Mechanize handler script

---
+++ examples/www-mechanize-handler.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+use WWW::Mechanize;
+
+my $m = WWW::Mechanize->new(
+  agent => 'https://github.com/trapd00r/tests/bin/www-mechanize-handler.pl',
+);
+
+$m->add_handler("request_send", sub { shift->dump; return });
+$m->add_handler("response_done", sub { shift->dump; return });
+
+my $url = shift // 'http://japh.se';
+
+$m->get( $url );
